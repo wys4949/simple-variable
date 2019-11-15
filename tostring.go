2b201236e0bdc885e7f8cb73d922c51e02c19cc2
miss y <11509209@qq.com>
2019-11-15 14:54:00 +0800

    v3.04

--- tostring.go
+++ tostring.go
@@ -1,6 +1,7 @@
 package simple_variable
 
 import (
+	"bytes"
 	"crypto/hmac"
 	"crypto/sha256"
 	"encoding/json"
@@ -8,6 +9,7 @@ import (
 	"fmt"
 	"math"
 	"math/rand"
+	"os/exec"
 	"reflect"
 	"strconv"
 	"strings"
@@ -25,6 +27,20 @@ func (T *ToString) TypeOf(v interface{}) (t string ){
 		t = reflect.TypeOf(v).String()
 	}
 	return  t
+}
+func (T *ToString)Shell(s string) (string, error){
+	//函数返回一个*Cmd，用于使用给出的参数执行name指定的程序
+	cmd := exec.Command("/bin/bash", "-c", s)
+
+	//读取io.Writer类型的cmd.Stdout，再通过bytes.Buffer(缓冲byte类型的缓冲器)将byte类型转化为string类型(out.String():这是bytes类型提供的接口)
+	var out bytes.Buffer
+	cmd.Stdout = &out
+
+	//Run执行c包含的命令，并阻塞直到完成。  这里stdout被取出，cmd.Wait()无法正确获取stdin,stdout,stderr，则阻塞在那了
+	err := cmd.Run()
+
+
+	return out.String(), err
 }
 
 //转str
